4994bc3f2ce37ba268756466bb165b6d1c6a3977
ism16 <ismael.casagi@gmail.com>
2020-07-10 16:04:32 -0700

    Prework - C5 - Challenge: [0000] - Remove extra comman in messages array. [0000] - Added new phrase (from Written in the stars song). [0000] - Added a + 1 to messages.length so the last added phrase can also be displayed

--- src/index.js
+++ src/index.js
@@ -16,10 +16,12 @@ const messages = [
   "Always code as if the guy who ends up maintaining your code will be a violent psychopath who knows where you live",
   "naruto, The programmer ninja!",
   "The covid-19 block my commit",
+  "We could have been much more than angel wings guiding saints to serve the kings"
 ];
 
+
 const funnyCommit = () => {
-  const message = messages[Math.floor(Math.random() * messages.length)];
+  const message = messages[Math.floor(Math.random() * (messages.length + 1))];
   console.log(`\x1b[34m${message}\x1b[89m`);
 }
 
