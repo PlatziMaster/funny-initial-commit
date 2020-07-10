83f246df4caa032b6fd773ba048fd2dfb68a21b0
Pedro Cruz <pcruzmay@yahoo.com>
2020-07-10 00:06:27 -0500

    funny Phrases pcruzmay

--- src/index.js
+++ src/index.js
@@ -16,6 +16,8 @@ const messages = [
   "Always code as if the guy who ends up maintaining your code will be a violent psychopath who knows where you live",
   "naruto, The programmer ninja!",
   "The covid-19 block my commit",
+  "Git me the power!",
+  "stupid is as stupid does"
 ];
 
 const funnyCommit = () => {
