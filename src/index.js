4cdb71af156c1725407240038db649123d306cbc
martino2197 <martino2197@gmail.com>
2020-07-10 22:52:28 -0500

    Add funny phrase

--- src/index.js
+++ src/index.js
@@ -16,6 +16,7 @@ const messages = [
   "Always code as if the guy who ends up maintaining your code will be a violent psychopath who knows where you live",
   "naruto, The programmer ninja!",
   "The covid-19 block my commit",
+  "Please tell me that I can make a good code, my standup career is very poor and I'm hungry"
 ];
 
 const funnyCommit = () => {
