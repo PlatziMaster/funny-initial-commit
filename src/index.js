9aa46f36483dc586f8ac685f3097df18e55aca68
Carlos Vargas <vermillion93@hotmail.com>
2020-07-11 02:50:36 -0500

    Github joke added

--- src/index.js
+++ src/index.js
@@ -16,6 +16,7 @@ const messages = [
   "Always code as if the guy who ends up maintaining your code will be a violent psychopath who knows where you live",
   "naruto, The programmer ninja!",
   "The covid-19 block my commit",
+  "In case of fire, git commit, git push and git outofthere",
 ];
 
 const funnyCommit = () => {
