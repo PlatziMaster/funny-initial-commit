53f0e28c1d97bfac6c7cc74b41a182a273611e96
Oscar Armando Tellez Covarrubias <zaroscar1984@gmail.com>
2020-07-10 22:00:35 -0500

    actualizado la frace de Oscar Armando Tellez

--- src/index.js
+++ src/index.js
@@ -16,6 +16,7 @@ const messages = [
   "Always code as if the guy who ends up maintaining your code will be a violent psychopath who knows where you live",
   "naruto, The programmer ninja!",
   "The covid-19 block my commit",
+  "The same thing we do every night, Pinky - try to take over the world!",
 ];
 
 const funnyCommit = () => {
