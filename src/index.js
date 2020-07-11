2fa25dea70b9493011ccd940b4db427d21648a4c
Ricardo Tornero <richardo@outlook.com>
2020-07-11 04:36:57 -0500

    one more phrase

--- src/index.js
+++ src/index.js
@@ -16,13 +16,14 @@ const messages = [
   "Always code as if the guy who ends up maintaining your code will be a violent psychopath who knows where you live",
   "naruto, The programmer ninja!",
   "The covid-19 block my commit",
+  "If you don't know what 'rm -rf' is for, just remember what did Thanos to the universe",
 ];
 
 const funnyCommit = () => {
   const message = messages[Math.floor(Math.random() * messages.length)];
   console.log(`\x1b[34m${message}\x1b[89m`);
-}
+};
 
 module.exports = {
-  funnyCommit
+  funnyCommit,
 };
