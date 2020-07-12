2171d4d4302e7af5cdf993cfd33acd4fe52aba1d
jrchamorro <jonathanrchamorro@gmail.com>
2020-07-11 20:03:23 -0500

    se agrega frase divertida

--- src/index.js
+++ src/index.js
@@ -16,6 +16,8 @@ const messages = [
   "Always code as if the guy who ends up maintaining your code will be a violent psychopath who knows where you live",
   "naruto, The programmer ninja!",
   "The covid-19 block my commit",
+  "My first contribution on github, I hope not to delete the Oscar repository",
+
 ];
 
 const funnyCommit = () => {
