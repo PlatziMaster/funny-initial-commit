8da502a79e212710567c0e99f64e3b0aba9b5109
Bryan Alejandro Sanchez Ramirez <sarabryanale@outlook.com>
2020-08-21 15:37:45 -0500

    It's my first day

--- src/index.js
+++ src/index.js
@@ -16,13 +16,14 @@ const messages = [
   "Always code as if the guy who ends up maintaining your code will be a violent psychopath who knows where you live",
   "naruto, The programmer ninja!",
   "The covid-19 block my commit",
+  "It's my first day",
 ];
 
 const funnyCommit = () => {
   const message = messages[Math.floor(Math.random() * messages.length)];
   console.log(`\x1b[34m${message}\x1b[89m`);
-}
+};
 
 module.exports = {
-  funnyCommit
+  funnyCommit,
 };
