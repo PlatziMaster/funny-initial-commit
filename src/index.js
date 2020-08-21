90ffbd8117b0a24cb231523885540d06b8cb49fb
Kevin J. Zea <60053714+KevinJZea@users.noreply.github.com>
2020-08-21 13:20:01 -0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -16,6 +16,7 @@ const messages = [
   "Always code as if the guy who ends up maintaining your code will be a violent psychopath who knows where you live",
   "naruto, The programmer ninja!",
   "The covid-19 block my commit",
+  "So, do you mean you usually use rebase and squash? And you dare to call yourself as a DevOps?!?",
 ];
 
 const funnyCommit = () => {
