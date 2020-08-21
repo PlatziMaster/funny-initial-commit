c65a1bd2b50560e45f8f2dbe702e42320d512a6e
JhonJBautista <jhon.bautista@ingenieros.com>
2020-08-21 15:54:15 -0500

    [C6] Reto Colaboración a Funny-Initial

--- src/index.js
+++ src/index.js
@@ -16,6 +16,7 @@ const messages = [
   "Always code as if the guy who ends up maintaining your code will be a violent psychopath who knows where you live",
   "naruto, The programmer ninja!",
   "The covid-19 block my commit",
+  "Vive el momento, siente... utiliza tu instinto y lanzate sin temor al fracazo - Begginer Python"
 ];
 
 const funnyCommit = () => {
