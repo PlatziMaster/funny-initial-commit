1d1e02b16aa628a61c998363b10357da9d9a8dc8
Diegoalesco95 <42728227+Diegoalesco95@users.noreply.github.com>
2020-08-21 11:18:57 -0500

    Challenge solved

--- src/index.js
+++ src/index.js
@@ -1,27 +1,28 @@
 const messages = [
-  "in case of fire: git commit, git push and leave building",
-  "This is where it all begins...",
-  "Commit committed",
-  "Version control is awful",
-  "COMMIT ALL THE FILES!",
-  "The same thing we do every night, Pinky - try to take over the world!",
-  "Lock S-foils in attack position",
-  "This commit is a lie",
+  'in case of fire: git commit, git push and leave building',
+  'This is where it all begins...',
+  'Commit committed',
+  'Version control is awful',
+  'COMMIT ALL THE FILES!',
+  'The same thing we do every night, Pinky - try to take over the world!',
+  'Lock S-foils in attack position',
+  'This commit is a lie',
   "I'll explain when you're older!",
-  "Here be Dragons",
-  "Reinventing the wheel. Again.",
-  "This is not the commit message you are looking for",
-  "Batman! (this commit has no parents)",
-  "the greats murmur as learning with this commit",
-  "Always code as if the guy who ends up maintaining your code will be a violent psychopath who knows where you live",
-  "naruto, The programmer ninja!",
-  "The covid-19 block my commit",
+  'Here be Dragons',
+  'Reinventing the wheel. Again.',
+  'This is not the commit message you are looking for',
+  'Batman! (this commit has no parents)',
+  'the greats murmur as learning with this commit',
+  'Always code as if the guy who ends up maintaining your code will be a violent psychopath who knows where you live',
+  'naruto, The programmer ninja!',
+  'The covid-19 block my commit',
+  'Oh no! It seems you have deleted system32'
 ];
 
 const funnyCommit = () => {
   const message = messages[Math.floor(Math.random() * messages.length)];
   console.log(`\x1b[34m${message}\x1b[89m`);
-}
+};
 
 module.exports = {
   funnyCommit
