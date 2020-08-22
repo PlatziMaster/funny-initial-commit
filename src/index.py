105e868df3d73bcf91fff8d9309c22633e30fce0
Xavier Carrera <xavier.carrera1990@gmail.com>
2020-08-22 11:30:15 -0500

    Python rocks!

---
+++ src/index.py
@@ -0,0 +1,29 @@
+import random
+
+messages = [
+    "in case of fire: git commit, git push and leave building",
+    "This is where it all begins...",
+    "Commit committed",
+    "Version control is awful",
+    "COMMIT ALL THE FILES!",
+    "The same thing we do every night, Pinky - try to take over the world!",
+    "Lock S-foils in attack position",
+    "This commit is a lie",
+    "I'll explain when you're older!",
+    "Here be Dragons",
+    "Reinventing the wheel. Again.",
+    "This is not the commit message you are looking for",
+    "Batman! (this commit has no parents)",
+    "the greats murmur as learning with this commit",
+    "Always code as if the guy who ends up maintaining your code will be a violent psychopath who knows where you live",
+    "naruto, The programmer ninja!",
+    "The covid-19 block my commit",
+]
+
+def funny_commit():
+    
+    message = random.choice(messages)
+    print(message)
+
+if __name__ == "__main__":
+    funny_commit()
